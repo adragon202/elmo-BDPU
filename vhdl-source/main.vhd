f2a84c78f790b63d193bd2496d421c4cf57cb60b
Alan Travis Irwin <alan.t.irwin@wmich.edu>
2016-02-28 16:49:14 -0500

    Added code for Single-Bit Adder

--- vhdl-source/main.vhd
+++ vhdl-source/main.vhd
@@ -1,14 +1,28 @@
--- main
+-- main 	Top level of design, incorporating all other modules in some degree
 
-architecture main of main is
+--Libraries
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity main is
+	generic(data_pins	: integer:= 64;
+			opcode_size	: integer:= 8;
+			core_count	: integer:= 4);
+	port(clk	: in std_logic;
+		opcode	: in std_logic_vector(opcode_size-1 downto 0);
+		data_in	: in std_logic_vector(data_pins-1 downto 0);
+		data_out: out std_logic_vector(data_pins-1 downto 0));
+end entity main;
+
+architecture main_a of main is
+
 begin
-	core: 	WORK.sub-core generic map()
+	core_1: WORK.sub_core generic map()
 			port map();
-	bram:	WORK.sub-bram generic map()
+	bram:	WORK.sub_bram generic map()
 			port map();
-	run: process
+	run: process(clk)
 	begin
 
 	end process;
-end main;
-
+end architecture main_a;
