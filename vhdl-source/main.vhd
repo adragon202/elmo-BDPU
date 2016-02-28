626053c68cfc837af48403260e92d5587e4cd7b0
Alan Travis Irwin <alan.t.irwin@wmich.edu>
2016-02-28 14:01:20 -0500

    outlined main.vhd

---
+++ vhdl-source/main.vhd
@@ -0,0 +1,14 @@
+-- main
+
+architecture main of main is
+begin
+	core: 	WORK.sub-core generic map()
+			port map();
+	bram:	WORK.sub-bram generic map()
+			port map();
+	run: process
+	begin
+
+	end process;
+end main;
+
