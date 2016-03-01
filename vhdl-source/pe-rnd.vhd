1f95180d509bb1f3f8bd47c5f5906f486ad41479
adragon202 <ithinkwemet@gmail.com>
2016-03-01 12:56:45 -0500

    Added code for Random Number Generation

---
+++ vhdl-source/pe-rnd.vhd
@@ -0,0 +1,17 @@
+-- pe-rnd 	A true Random Number Generator, used to optimize the process of random number generation
+--			Number produced is truely random as it is not an element of time.
+
+entity rnd is
+	port(x : in std_logic;
+		A : out std_logic);
+end entity;
+
+architecture beh of rnd is
+	signal Afdbck: std_logic;
+begin
+	rand:process
+	begin
+		Afdbck <= not (not ((not x and Afdbck) or (x and not Afdbck)));
+		A <= Afdbck;
+	end process;
+end architecture;
