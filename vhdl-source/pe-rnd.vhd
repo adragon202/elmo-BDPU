fa8bc7c7f50b25559b1435e8f4caeb32368f0607
adragon202 <ithinkwemet@gmail.com>
2016-03-03 14:50:24 -0500

    added ieee libraries to pe-rnd

--- vhdl-source/pe-rnd.vhd
+++ vhdl-source/pe-rnd.vhd
@@ -1,6 +1,10 @@
 -- pe-rnd 	A true Random Number Generator, used to optimize the process of random number generation
 --			Number produced is truely random as it is not an element of time.
 
+--Libraries
+library IEEE;
+use IEEE.std_logic_1164.all;
+
 entity rnd is
 	port(x : in std_logic;
 		A : out std_logic);
