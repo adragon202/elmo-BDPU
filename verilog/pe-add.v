0798573cb4847b7f8817678013cef4799cb0f5b8
mcowley15 <mcowley15@gmail.com>
2016-06-06 17:24:52 -0400

    Update pe-add.v

--- verilog/pe-add.v
+++ verilog/pe-add.v
@@ -63,7 +63,7 @@ module adder16( a, b, cin, sum, cout, PG, GG);
 	wire [3:0] C, G, P;
 	//code starts here
 	//Carry Look-Ahead
-	assign C[0] = G[0] | (P[0] & C[0]);
+	assign C[0] = G[0] | (P[0] & cin);
 	assign C[1] = G[1] | (P[1] & G[0]) | (P[1] & P[0] & C[0]);
 	assign C[2] = G[2] | (P[2] & G[1]) | (P[2] & P[1] & G[0]) | (P[2] & P[1] & P[0] & C[0]);
 	assign C[3] = G[3] | (P[3] & G[2]) | (P[3] & P[2] & G[1]) | (P[3] & P[2] & P[1] & G[0]) | (P[3] & P[2] & P[1] & P[0] & C[0]);
