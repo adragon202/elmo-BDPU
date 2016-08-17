3874da344696e2d9935d52d9897deee1659ff1d1
adragon202 <ithinkwemet@gmail.com>
2016-08-17 16:35:20 -0400

    32-bit Floating Point Adder will add positive values. Signed-off-by: adragon202 <ithinkwemet@gmail.com>

--- verilog/pe-addFloat.v
+++ verilog/pe-addFloat.v
@@ -1,5 +1,5 @@
 
-module add_f32(a,b,sum);
+module add_f32(a, b, sum, a_greater, exp_diff, sum_exp, mant_sum);
 	localparam WIDTH = 32;
 	localparam EXPONENTWIDTH = 8;
 	localparam MANTISSAWIDTH = 23;
@@ -7,64 +7,73 @@ module add_f32(a,b,sum);
 	input [WIDTH - 1:0] a, b;
 	// outputs
 	output [WIDTH - 1:0] sum;
+	output a_greater;
+	output [EXPONENTWIDTH - 1:0] exp_diff;
+	output [MANTISSAWIDTH - 1:0] mant_sum;
+	output [EXPONENTWIDTH - 1:0] sum_exp;
 	// internal wires
 	wire a_sign							= a[WIDTH - 1];
 	wire [EXPONENTWIDTH - 1:0] a_exp	= a[WIDTH - 2:MANTISSAWIDTH];
 	wire [MANTISSAWIDTH - 1:0] a_mant	= a[MANTISSAWIDTH-1:0];
-	wire b_sign						= b[WIDTH - 1];
+	wire b_sign							= b[WIDTH - 1];
 	wire [EXPONENTWIDTH - 1:0] b_exp	= b[WIDTH - 2:MANTISSAWIDTH];
 	wire [MANTISSAWIDTH - 1:0] b_mant	= b[MANTISSAWIDTH-1:0];
 	wire [EXPONENTWIDTH - 1:0] exp_diff;
 	wire [EXPONENTWIDTH - 1:0] exp_neg;
 	wire [MANTISSAWIDTH - 1:0] mant_sum;
 	wire a_greater; //indicates if a is greater or lesser than b
-	reg in_sign;
+	wire a_exp_greater;
+	reg in1_sign;
+	reg in2_sign;
 	reg [EXPONENTWIDTH - 1:0] in_exp;
 	reg [MANTISSAWIDTH - 1:0] in1_mant;
+	wire [MANTISSAWIDTH - 1:0] in1_mant_neg;
 	reg [MANTISSAWIDTH - 1:0] in2_mant;
+	wire [MANTISSAWIDTH - 1:0] in2_mant_neg;
 	reg sum_sign;
 	wire [EXPONENTWIDTH - 1:0] sum_exp;
 	reg [MANTISSAWIDTH - 1:0] sum_mant;
 	wire sum_mant_carry;
-	reg i;
 
 	//Evalute the greater of a_exp or b_exp so that a > b
 	//If b is negative and a is not
 	//If b exponent is less than a
 	//If b mantissa is less than a
-	assign a_greater = (a_sign != b_sign && a_sign == 0) || ((a_exp > b_exp && a_sign == 0) || (a_exp < b_exp && a_sign == 1)) || ((a_mant > b_mant && a_sign == 0) || (a_mant < b_mant && a_sign == 1));
+	assign a_exp_greater = (a_exp > b_exp);
+	assign a_greater = (a_sign != b_sign && a_sign == 0) || ((a_exp > b_exp && a_sign == 0) || (a_exp < b_exp && a_sign == 1)) || ((a_mant > b_mant && a_sign == 0 && a_exp == b_exp) || (a_mant < b_mant && a_sign == 1 && a_exp == b_exp));
 
 	//Consider b_exp = a_exp
 	//Shift b_mant to the right by a_exp - b_exp
-	adder8 add8_expneg(.a((a_greater == 1) ? ~(b_exp) :  ~(a_exp)), .b(1), .cin(1'b0), .sum(exp_neg), .cout(), .PG(), .GG());
-	adder8 add8_expdiff(.a((a_greater == 1) ? a_exp : exp_neg), .b((a_greater == 0) ? b_exp : exp_neg), .cin(1'b0), .sum(exp_diff), .cout(), .PG(), .GG());
+	adder8 add8_expneg(.a((a_exp_greater == 1) ? ~(b_exp) :  ~(a_exp)), .b(1), .cin(1'b0), .sum(exp_neg), .cout(), .PG(), .GG());
+	adder8 add8_expdiff(.a((a_exp_greater == 1) ? a_exp : exp_neg), .b((a_exp_greater == 0) ? b_exp : exp_neg), .cin(1'b1), .sum(exp_diff), .cout(), .PG(), .GG());
 	always @(exp_diff) begin
 		//Assign registers for remainder of calculations
-		in_sign <= (a_greater == 1) ? a_sign : b_sign;
+		in1_sign <= (a_greater == 1) ? a_sign : b_sign;
+		in2_sign <= (a_greater == 0) ? a_sign : b_sign;
 		in_exp	 <= (a_greater == 1) ? a_exp  : b_exp; //After this step exponents will be equal
-		in1_mant <= (a_greater == 1) ? a_mant : b_mant;
-		in2_mant <= (a_greater == 0) ? a_mant : b_mant;
+		in1_mant <= ((a_greater == 1) ? a_mant : b_mant) >> 1;
 		//Shift in2 as the lesser value
-		for (i = 0; i < exp_diff; i = i + 1)
-		begin:mantissa_shift
-			in2_mant = in2_mant >> 1;
-		end
+		in2_mant <= ((a_greater == 0) ? a_mant : b_mant) >> exp_diff;
 	end
 
-	//Sum mantissa's (Apply negative to mantissa)
-	adder24 add24_mantsum(.a({1'b0,in1_mant}), .b({1'b0,in2_mant}), .cin(1'b0), .sum(mant_sum), .cout(sum_mant_carry));
+	//Create Negative formats for proper addition
+	adder24 add24_mant1_neg(.a({1'b1,~(in1_mant)}), .b({24'd1}), .cin(0), .sum({in1_mant_neg}));
+	adder24 add24_mant2_neg(.a({1'b1,~(in2_mant)}), .b({24'd1}), .cin(0), .sum({in2_mant_neg}));
+	//Sum mantissa's
+	adder24 add24_mantsum(.a((in1_sign == 0) ? {1'b0,in1_mant} : {1'b1,in1_mant_neg}), .b((in1_sign == 0) ? {1'b0,in2_mant} : {1'b1,in2_mant_neg}), .cin(1'b0), .sum(mant_sum), .cout(sum_mant_carry));
 
 	//Normalize
 	//Adjust resulting Exponent
-	adder8 add8_sumexp(.a(in_exp), .b({7'd0,(sum_mant_carry == 1) ? 1 : 0}), .cin(1'b0), .sum(sum_exp));
+	adder8 add8_sumexp(.a(in_exp), .b({7'b0,(sum_mant_carry == 1)}), .cin(1'b0), .sum(sum_exp));
 	always @(mant_sum) begin
-		sum_mant <= mant_sum;
 		if (sum_mant_carry == 1) begin
-			sum_mant = sum_mant >> 1;
+			sum_mant <= mant_sum >> 1;
 			sum_mant[MANTISSAWIDTH-1] <= 1;
+		end else begin
+			sum_mant <= mant_sum;
 		end
 	end
 
-	assign sum = {1'b0, sum_exp, sum_mant};
+	assign sum = ((a_greater == 1 && a_sign == 1) || (a_greater == 0 && b_sign == 1)) ? {1'b1, sum_exp, sum_mant} : {1'b0, sum_exp, sum_mant};
 
 endmodule //add_f32
