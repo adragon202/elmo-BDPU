f25be9813a3b213cd6c16a246522ab09835adb31
mcowley15 <mcowley15@gmail.com>
2016-08-26 21:23:38 -0400

    This module works for this testbench. We should do more tests though.

--- verilog/pe-addFloat.v
+++ verilog/pe-addFloat.v
@@ -1,7 +1,7 @@
 
 //Rounding error will occur, causing some results to be off by 1 in hex or binary,
 //Error will only be to the last decimal place.
-module add_f32(a, b, sum, a_greater, exp_diff, sum_exp, sum_mant, mant_sum, out1_mant, out2_mant);
+module add_f32(a, b, sum, exp_diff, sum_exp, in_exp, sum_mant, mant_sum_shift, mant_sum, out1_mant, out2_mant);
 	localparam WIDTH = 32;
 	localparam EXPONENTWIDTH = 8;
 	localparam MANTISSAWIDTH = 24; //Input and output Mantissa will be 23 bits
@@ -11,13 +11,14 @@ module add_f32(a, b, sum, a_greater, exp_diff, sum_exp, sum_mant, mant_sum, out1
 	output [WIDTH - 1:0] sum;
 	//*********************************************************************
 	//For Debug Purposes
-	output a_greater;
 	output [EXPONENTWIDTH - 1:0] exp_diff;
 	output [MANTISSAWIDTH - 1:0] mant_sum;
 	output [MANTISSAWIDTH - 1:0] sum_mant;
 	output [EXPONENTWIDTH - 1:0] sum_exp;
+	output [EXPONENTWIDTH - 1:0] in_exp;
 	output [MANTISSAWIDTH - 1:0] out1_mant;
 	output [MANTISSAWIDTH - 1:0] out2_mant;
+	output [4:0] mant_sum_shift;
 	assign out1_mant = (!in1_sign) ? {1'b0,in1_mant} : {1'b1,in1_mant_neg};
 	assign out2_mant = (!in2_sign) ? {1'b0,in2_mant} : {1'b1,in2_mant_neg};
 	//*********************************************************************
@@ -31,8 +32,10 @@ module add_f32(a, b, sum, a_greater, exp_diff, sum_exp, sum_mant, mant_sum, out1
 	wire [EXPONENTWIDTH - 1:0] exp_diff;
 	wire [EXPONENTWIDTH - 1:0] exp_neg;
 	wire [MANTISSAWIDTH - 1:0] mant_sum;
+	wire [4:0] mant_sum_shift;
 	wire [MANTISSAWIDTH - 1:0] mant_sum_neg;
-	wire a_greater; //indicates if a is greater or lesser than b
+	wire mant_sum_sign;
+	wire a_abs_greater;
 	wire a_exp_greater;
 	wire in1_sign;
 	wire in2_sign;
@@ -41,10 +44,12 @@ module add_f32(a, b, sum, a_greater, exp_diff, sum_exp, sum_mant, mant_sum, out1
 	wire [MANTISSAWIDTH - 1:0] in1_mant_neg;
 	reg [MANTISSAWIDTH - 1:0] in2_mant;
 	wire [MANTISSAWIDTH - 1:0] in2_mant_neg;
-	reg sum_sign;
 	wire [EXPONENTWIDTH - 1:0] sum_exp;
 	reg [MANTISSAWIDTH - 1:0] sum_mant;
 	wire sum_mant_carry;
+	reg rslt_sign;
+	reg [EXPONENTWIDTH - 1:0] rslt_exp;
+	reg [MANTISSAWIDTH - 2:0] rslt_mant;
 
 
 	//Evalute the greater of a_exp or b_exp so that a > b
@@ -52,56 +57,127 @@ module add_f32(a, b, sum, a_greater, exp_diff, sum_exp, sum_mant, mant_sum, out1
 	//If b exponent is less than a
 	//If b mantissa is less than a
 	assign a_exp_greater = (a_exp > b_exp);
-	assign a_greater = (a_sign != b_sign && a_sign == 0) || ((a_exp > b_exp && a_sign == 0) || (a_exp < b_exp && a_sign == 1)) || ((a_mant > b_mant && a_sign == 0 && a_exp == b_exp) || (a_mant < b_mant && a_sign == 1 && a_exp == b_exp));
+	assign a_abs_greater = (a_exp > b_exp) || (a_mant > b_mant && a_exp == b_exp);
 
 	//Consider b_exp = a_exp
 	//Shift b_mant to the right by a_exp - b_exp
-	adder8 add8_expneg(.a((a_exp_greater == 1) ? ~(b_exp) :  ~(a_exp)),
+	adder8 add8_expneg(.a((a_exp_greater) ? ~(b_exp) :  ~(a_exp)),
 						.b(8'b0),
 						.cin(1'd1),
 						.sum(exp_neg));
-	adder8 add8_expdiff(.a((a_exp_greater == 1) ? a_exp : exp_neg),
-						.b((a_exp_greater == 0) ? b_exp : exp_neg),
+	adder8 add8_expdiff(.a((a_exp_greater) ? a_exp : exp_neg),
+						.b((!a_exp_greater) ? b_exp : exp_neg),
 						.cin(1'd0),
 						.sum(exp_diff));
-	assign in1_sign = (a_greater) ? a_sign : b_sign;
-	assign in2_sign = (!a_greater) ? a_sign : b_sign;
+	assign in1_sign = (a_exp_greater) ? a_sign : b_sign;
+	assign in2_sign = (!a_exp_greater) ? a_sign : b_sign;
 	always @(*) begin
 		//Assign registers for remainder of calculations
-		in_exp	 <= (a_greater == 1) ? a_exp  : b_exp; //After this step exponents will be equal
+		in_exp	 <= (a_exp_greater) ? a_exp  : b_exp; //After this step exponents will be equal
 		//Shift in2 as the lesser value
 		if (exp_diff == 0) begin
-			in1_mant <= ((a_greater == 1) ? a_mant : b_mant) >> 1;
-			in2_mant <= ((a_greater == 0) ? a_mant : b_mant) >> 1;
+			in1_mant <= ((a_exp_greater) ? a_mant : b_mant);
+			in2_mant <= ((!a_exp_greater) ? a_mant : b_mant);
 		end else begin
-			in1_mant <= ((a_greater == 1) ? a_mant : b_mant);
-			in2_mant <= ((a_greater == 0) ? a_mant : b_mant) >> (exp_diff);
+			in1_mant <= ((a_exp_greater) ? a_mant : b_mant);
+			in2_mant <= ((!a_exp_greater) ? a_mant : b_mant) >> (exp_diff);
 		end
 	end
 
 	//Create Negative formats for proper addition
-	adder24 add24_mant1_neg(.a(~(in1_mant)), .b({23'b0,1'b1}), .cin(1'd0), .sum(in1_mant_neg));
-	adder24 add24_mant2_neg(.a(~(in2_mant)), .b({23'b0,1'b1}), .cin(1'd0), .sum(in2_mant_neg));
+	adder24 add24_mant1_neg(.a(~(in1_mant)), .b(24'b0), .cin(1'd1), .sum(in1_mant_neg));
+	adder24 add24_mant2_neg(.a(~(in2_mant)), .b(24'b0), .cin(1'd1), .sum(in2_mant_neg));
 	//Sum mantissa's
-	adder24 add24_mantsum(.a((in1_sign == 0) ? in1_mant : in1_mant_neg),
-							.b((in2_sign == 0) ? in2_mant : in2_mant_neg),
+	adder24 add24_mantsum(.a((!in1_sign) ? in1_mant : in1_mant_neg),
+							.b((!in2_sign) ? in2_mant: in2_mant_neg),
 							.cin(1'b0),
 							.sum(mant_sum), .cout(sum_mant_carry));
 
 	//Normalize
 	//Adjust resulting Exponent
+	leading_zeroes_25 leading_zeroes_mant((mant_sum_sign ? mant_sum_neg : mant_sum), mant_sum_shift);
 	//If the exponent difference was only 0 or 1, then the exponent increments or decrements depending on the greater values sign
 	adder8 add8_sumexp(.a(in_exp),
-						.b({7'b0,(a_exp == b_exp) ? 1'd1 : sum_mant_carry}),
-						.cin(1'b0),
+						.b((sum_mant_carry && a_sign == 0 && b_sign == 0) || (!sum_mant_carry && a_sign == 1 && b_sign == 1) ? 8'd1 : 
+							 {3'b111,~mant_sum_shift}),
+						.cin((sum_mant_carry && a_sign == 0 && b_sign == 0) || (!sum_mant_carry && a_sign == 1 && b_sign == 1) ? 1'b0 : 1'b1),
 						.sum(sum_exp));
 	//Undo two's compliment on mantissa when the result is negative.
-	adder24 add24_mantsum_neg(.a(~(mant_sum)), .b({23'b0,1'b1}), .cin(1'd0), .sum(mant_sum_neg));
+	adder24 add24_mantsum_neg(.a(~(mant_sum)), .b(24'b0), .cin(1'd1), .sum(mant_sum_neg));
+	assign mant_sum_sign = ((in1_sign && in1_mant > in2_mant) || (in2_sign && in1_mant < in2_mant));
 
 	always @(*) begin
-		sum_mant <= mant_sum;
+		if ((sum_mant_carry && a_sign == 0 && b_sign == 0) || (!sum_mant_carry && a_sign == 1 && b_sign == 1))
+			sum_mant <= ((mant_sum_sign) ? {1'b1, mant_sum_neg[MANTISSAWIDTH-1:1]} : {1'b1, mant_sum[MANTISSAWIDTH-1:1]});
+		else 
+			sum_mant <= ((mant_sum_sign) ? {mant_sum_neg} : mant_sum) << mant_sum_shift;
 	end
 
-	assign sum = ((a_greater == 1 && a_sign == 1) || (a_greater == 0 && b_sign == 1)) ? {1'b1, sum_exp, sum_mant[MANTISSAWIDTH-2:0]} : {1'b0, sum_exp, sum_mant[MANTISSAWIDTH-2:0]};
+	//Evalute final result
+	// assign rslt_sign = (a_sign != b_sign && a_exp == b_exp && a_mant == b_mant) ? 1'd0 : //Special Case Zero
+	// 					(a_abs_greater && a_sign) || (!a_abs_greater && b_sign);
+	// assign rslt_exp = (a_sign != b_sign && a_exp == b_exp && a_mant == b_mant) ? 8'd0 : sum_exp; //Special Case Zero
+	// assign rslt_mant = sum_mant[MANTISSAWIDTH-2:0];
+
+	always @(*) begin 
+		if (a == 0) begin 
+			rslt_sign = b_sign;
+			rslt_exp = b_exp;
+			rslt_mant	= b_mant;
+		end 
+		else if (b == 0) begin 
+			rslt_sign = a_sign;
+			rslt_exp = a_exp;
+			rslt_mant	= a_mant;
+		end 
+		else if (a_sign != b_sign && a_exp == b_exp && a_mant == b_mant) begin
+			rslt_sign = 0;
+			rslt_exp = 0;
+			rslt_mant = 0;
+		end 
+		else begin 
+			rslt_sign = (a_abs_greater && a_sign) || (!a_abs_greater && b_sign);
+			rslt_exp = sum_exp;
+			rslt_mant = sum_mant[MANTISSAWIDTH-2:0];
+		end 
+	end
+
+	assign sum = {rslt_sign, rslt_exp, rslt_mant};
 
 endmodule //add_f32
+
+module leading_zeroes_25(a, count);
+	localparam WIDTH = 24;
+	localparam MAX_COUNT_WIDTH = 5;
+	// inputs
+	input [WIDTH - 1:0] a;
+	// outputs
+	output [MAX_COUNT_WIDTH - 1:0] count;
+
+	assign count = (a[WIDTH - 1] == 1) ? 0 :
+		(a[WIDTH - 1:23] == 0 && a[22] == 1) ? 1 :
+		(a[WIDTH - 1:22] == 0 && a[21] == 1) ? 2 :
+		(a[WIDTH - 1:21] == 0 && a[20] == 1) ? 3 :
+		(a[WIDTH - 1:20] == 0 && a[19] == 1) ? 4 :
+		(a[WIDTH - 1:19] == 0 && a[18] == 1) ? 5 :
+		(a[WIDTH - 1:18] == 0 && a[17] == 1) ? 6 :
+		(a[WIDTH - 1:17] == 0 && a[16] == 1) ? 7 :
+		(a[WIDTH - 1:16] == 0 && a[15] == 1) ? 8 :
+		(a[WIDTH - 1:15] == 0 && a[14] == 1) ? 9 :
+		(a[WIDTH - 1:14] == 0 && a[13] == 1) ? 10 :
+		(a[WIDTH - 1:13] == 0 && a[12] == 1) ? 11 :
+		(a[WIDTH - 1:12] == 0 && a[11] == 1) ? 12 :
+		(a[WIDTH - 1:11] == 0 && a[10] == 1) ? 13 :
+		(a[WIDTH - 1:10] == 0 && a[9] == 1) ? 14 :
+		(a[WIDTH - 1:9] == 0 && a[8] == 1) ? 15 :
+		(a[WIDTH - 1:8] == 0 && a[7] == 1) ? 16 :
+		(a[WIDTH - 1:7] == 0 && a[6] == 1) ? 17 :
+		(a[WIDTH - 1:6] == 0 && a[5] == 1) ? 18 :
+		(a[WIDTH - 1:5] == 0 && a[4] == 1) ? 19 :
+		(a[WIDTH - 1:4] == 0 && a[3] == 1) ? 20 :
+		(a[WIDTH - 1:3] == 0 && a[2] == 1) ? 21 :
+		(a[WIDTH - 1:2] == 0 && a[1] == 1) ? 22 :
+		(a[WIDTH - 1:1] == 0 && a[0] == 1) ? 23 :
+		24;
+
+endmodule //leading_zeroes_25
--- verilog/pe-addFloat_tb.v
+++ verilog/pe-addFloat_tb.v
@@ -4,56 +4,195 @@ module test;
 	localparam MANTISSAWIDTH = 23;
 
 	initial begin
-		// 1.5 + 0.25 = 1.75 (1071644672)
-		# 1	a_sign	= 0;
-		# 0	a_exp	= 8'd 127;
-		# 0	a_mant	= 23'd 4194304;
-		# 0	b_sign	= 0;
-		# 0	b_exp	= 8'd 125;
-		# 0	b_mant	= 23'd 0;
-		// 1.5 - 0.25 = 1.25 (1067450368)
-		# 1	a_sign	= 0;
-		# 0	a_exp	= 8'd 127;
-		# 0	a_mant	= 23'd 4194304;
-		# 0	b_sign	= 1;
-		# 0	b_exp	= 8'd 125;
-		# 0	b_mant	= 23'd 0;
-		// 1.5 + 2.5 = 4 (1082130432)
-		# 1	a_sign	= 0;
-		# 0	a_exp	= 8'd 127;
-		# 0	a_mant	= 23'd 4194304;
-		# 0	b_sign	= 0;
-		# 0	b_exp	= 8'd 128;
-		# 0	b_mant	= 23'd 2097152;
-		// 1.5 - 2.5 = -1 (3212836864)
-		# 1	a_sign	= 0;
-		# 0	a_exp	= 8'd 127;
-		# 0	a_mant	= 23'd 4194304;
-		# 0	b_sign	= 1;
-		# 0	b_exp	= 8'd 128;
-		# 0	b_mant	= 23'd 2097152;
-		// 20.8 + 1.2 = 22 (1102053376)
-		# 1	a_sign	= 0;
-		# 0	a_exp	= 8'd 131;
-		# 0	a_mant	= 23'd 2516582;
-		# 0	b_sign	= 0;
-		# 0	b_exp	= 8'd 127;
-		# 0	b_mant	= 23'd 1677722;
-		// 20.8 - 1.2 = 19.6 (1100795085)
-		# 1	a_sign	= 0;
-		# 0	a_exp	= 8'd 131;
-		# 0	a_mant	= 23'd 2516582;
-		# 0	b_sign	= 1;
-		# 0	b_exp	= 8'd 127;
-		# 0	b_mant	= 23'd 1677722;
-		// -20.8 - 1.2 = -22 (3249537024)
+		// 01, 1.5 + 0.25 = 1.75 (1071644672)
+		# 1	a_sign = 0;
+		# 0	a_exp = 8'd 127;
+		# 0	a_mant = 23'd 4194304;
+		# 0 b_sign = 0;
+		# 0 b_exp = 8'd 125;
+		# 0 b_mant = 23'd 0;
+		// 02, 1.5 - 0.25 = 1.25 (1067450368)
+		# 1	a_sign = 0;
+		# 0	a_exp = 8'd 127;
+		# 0	a_mant = 23'd 4194304;
+		# 0 b_sign = 1;
+		# 0 b_exp = 8'd 125;
+		# 0 b_mant = 23'd 0;
+		// 03, 1.5 + 2.5 = 4 (1082130432)
+		# 1	a_sign = 0;
+		# 0	a_exp = 8'd 127;
+		# 0	a_mant = 23'd 4194304;
+		# 0 b_sign = 0;
+		# 0 b_exp = 8'd 128;
+		# 0 b_mant = 23'd 2097152;
+		// 04, 1.5 - 2.5 = -1 (3212836864)
+		# 1	a_sign = 0;
+		# 0	a_exp = 8'd 127;
+		# 0	a_mant = 23'd 4194304;
+		# 0 b_sign = 1;
+		# 0 b_exp = 8'd 128;
+		# 0 b_mant = 23'd 2097152;
+		// 05, 20.8 + 1.2 = 22 (1102053376)
+		# 1	a_sign = 0;
+		# 0	a_exp = 8'd 131;
+		# 0	a_mant = 23'd 2516582;
+		# 0 b_sign = 0;
+		# 0 b_exp = 8'd 127;
+		# 0 b_mant = 23'd 1677722;
+		// 06, 20.8 - 1.2 = 19.6 (1100795085)
+		# 1	a_sign = 0;
+		# 0	a_exp = 8'd 131;
+		# 0	a_mant = 23'd 2516582;
+		# 0 b_sign = 1;
+		# 0 b_exp = 8'd 127;
+		# 0 b_mant = 23'd 1677722;
+		// 07, -20.8 - 1.2 = -22 (3249537024)
+		# 1	a_sign = 1;
+		# 0	a_exp = 8'd 131;
+		# 0	a_mant = 23'd 2516582;
+		# 0 b_sign = 1;
+		# 0 b_exp = 8'd 127;
+		# 0 b_mant = 23'd 1677722;
+		// 08, -500.12 + 500.12 = 0 (0)
 		# 1	a_sign	= 1;
-		# 0	a_exp	= 8'd 131;
-		# 0	a_mant	= 23'd 2516582;
+		# 0	a_exp	= 8'd 135;
+		# 0	a_mant	= 23'd 7999324;
+		# 0	b_sign	= 0;
+		# 0	b_exp	= 8'd 135;
+		# 0	b_mant	= 23'd 7999324;
+		// 09, 500.12 - 500.12 = 0 (0)
+		# 1	a_sign	= 0;
+		# 0	a_exp	= 8'd 135;
+		# 0	a_mant	= 23'd 7999324;
 		# 0	b_sign	= 1;
-		# 0	b_exp	= 8'd 127;
-		# 0	b_mant	= 23'd 1677722;
-
+		# 0	b_exp	= 8'd 135;
+		# 0	b_mant	= 23'd 7999324;
+		// 10, -500.12 + 700.12 = 200 (43480000)	
+		# 1	a_sign	= 1;
+		# 0	a_exp	= 8'd 135;
+		# 0	a_mant	= 23'd 7999324;
+		# 0	b_sign	= 0;
+		# 0	b_exp	= 8'd 136;
+		# 0	b_mant	= 23'd 3082158;
+		// 11, 700.12 - 500.12 = 200 (43480000)
+		# 1	a_sign	= 0;
+		# 0	a_exp	= 8'd 136;
+		# 0	a_mant	= 23'd 3082158;
+		# 0	b_sign	= 1;
+		# 0	b_exp	= 8'd 135;
+		# 0	b_mant	= 23'd 7999324;
+		// 12, 0 + 0 = 0 (0x0)
+		# 1	a_sign = 0;
+		# 0	a_exp = 8'd 0;
+		# 0	a_mant = 23'd0;
+		# 0 b_sign = 0;
+		# 0 b_exp = 8'd0;
+		# 0 b_mant = 23'd0;
+		// 13, 5 + 0 = 5 (0x40a00000)
+		# 1	a_sign = 0;
+		# 0	a_exp = 8'd129;
+		# 0	a_mant = 23'd2097152;
+		# 0 b_sign = 0;
+		# 0 b_exp = 8'd0;
+		# 0 b_mant = 23'd0;
+		// 14, 0 - 6 = -6 (0x40c00000)
+		# 1	a_sign = 1;
+		# 0	a_exp = 8'd129;
+		# 0	a_mant = 23'd4194304;
+		# 0 b_sign = 0;
+		# 0 b_exp = 8'd0;
+		# 0 b_mant = 23'd0;
+		// 15, 1.25 - 1 = 0.25 (0x3e800000)
+		# 1	a_sign = 0;
+		# 0	a_exp = 8'd127;
+		# 0	a_mant = 23'd2097152;
+		# 0 b_sign = 1;
+		# 0 b_exp = 8'd127;
+		# 0 b_mant = 23'd0;
+		// 16, -1.25 + 1 = -0.25 (0xbe800000)
+		# 1	a_sign = 1;
+		# 0	a_exp = 8'd127;
+		# 0	a_mant = 23'd2097152;
+		# 0 b_sign = 0;
+		# 0 b_exp = 8'd127;
+		# 0 b_mant = 23'd0;
+		// 17, 0.24491003 + 0.2570581 = 0.50196815 (0x3f0080fc)
+		# 1	a_sign = 0;
+		# 0	a_exp = 8'd124;
+		# 0	a_mant = 23'd8047026;
+		# 0 b_sign = 0;
+		# 0 b_exp = 8'd125;
+		# 0 b_mant = 23'd236831;
+		// 18, 0.9398058	- 0.1159356976 = 0.8238701024 (0x3f52e927)
+		# 1	a_sign = 0;
+		# 0	a_exp = 8'd126;
+		# 0	a_mant = 23'd7378717;
+		# 0 b_sign = 1;
+		# 0 b_exp = 8'd123;
+		# 0 b_mant = 23'd7172018;
+		// 19, 4.98086	+ 0.2821059162 = 5.2629659162 (0x40a86a37)
+		# 1	a_sign = 0;
+		# 0	a_exp = 8'd129;
+		# 0	a_mant = 23'd2057013;
+		# 0 b_sign = 0;
+		# 0 b_exp = 8'd125;
+		# 0 b_mant = 23'd1077296;
+		// 20, 785.7069876	- 30.88526349 = 754.82172411 (0x443cb497)
+		# 1	a_sign = 0;
+		# 0	a_exp = 8'd136;
+		# 0	a_mant = 23'd4484415;
+		# 0 b_sign = 1;
+		# 0 b_exp = 8'd131;
+		# 0 b_mant = 23'd7804165;
+		// 21, 6.8311649E9	- 1.34663962E9 = 5.4845251E9 (0x4fa3739b)
+		# 1	a_sign = 0;
+		# 0	a_exp = 8'd159;
+		# 0	a_mant = 23'd4953511;
+		# 0 b_sign = 1;
+		# 0 b_exp = 8'd157;
+		# 0 b_mant = 23'd2132014;
+		// 22, 4.9249485E9 - 0.9940168 = 4.9249485E9 (0x4f92c660)
+		# 1	a_sign = 0;
+		# 0	a_exp = 8'd159;
+		# 0	a_mant = 23'd1230432;
+		# 0 b_sign = 1;
+		# 0 b_exp = 8'd126;
+		# 0 b_mant = 23'd8288227;
+		// 23, 0.5747798 -	7339695.0 = -7339694.42522 (0xcadffd5d)
+		# 1	a_sign = 0;
+		# 0	a_exp = 8'd126;
+		# 0	a_mant = 23'd1254597;
+		# 0 b_sign = 1;
+		# 0 b_exp = 8'd149;
+		# 0 b_mant = 23'd6290782;
+		// 24, -0.84127694 - 0.7590595 = -1.6003364 (0xbfccd7d3)
+		# 1	a_sign = 1;
+		# 0	a_exp = 8'd126;
+		# 0	a_mant = 23'd5725677;
+		# 0 b_sign = 1;
+		# 0 b_exp = 8'd126;
+		# 0 b_mant = 23'd4346297;
+		// 25, 0.26604247	- 0.26604247 = 0 (0x00000000)
+		# 1	a_sign = 0;
+		# 0	a_exp = 8'd125;
+		# 0	a_mant = 23'd538296;
+		# 0 b_sign = 1;
+		# 0 b_exp = 8'd125;
+		# 0 b_mant = 23'd538296;
+		// 26, -288.4304631 - 778.925073 = -1067.3556(0xc4856b61)
+		# 1	a_sign = 1;
+		# 0	a_exp = 8'd135;
+		# 0	a_mant = 23'd1062681;
+		# 0 b_sign = 1;
+		# 0 b_exp = 8'd136;
+		# 0 b_mant = 23'd4373300;
+		// 27, -3.55458611E9	- 5.8136832E9 = -9.3682688E9 (0xd00b991c)
+		# 1	a_sign = 1;
+		# 0	a_exp = 8'd158;
+		# 0	a_mant = 23'd5496494;
+		# 0 b_sign = 1;
+		# 0 b_exp = 8'd159;
+		# 0 b_mant = 23'd2966242;
 		# 10 $stop;
 	end
 
@@ -67,20 +206,21 @@ module test;
 	reg [MANTISSAWIDTH - 1:0] b_mant = 23'd500;
 	wire [WIDTH - 1:0] b = {b_sign, b_exp, b_mant}; //1.0000596
 	wire [WIDTH - 1:0] sum;
-	wire a_greater;
 	wire [EXPONENTWIDTH - 1:0] exp_diff;
 	wire [EXPONENTWIDTH - 1:0] rsltexp;
+	wire [EXPONENTWIDTH - 1:0] prersltexp;
+	wire [4:0] mant_shift;
 	wire [MANTISSAWIDTH - 1:0] rsltmant;
 	wire [MANTISSAWIDTH:0] prersltmant;
 	wire [MANTISSAWIDTH:0] in1_mant;
 	wire [MANTISSAWIDTH:0] in2_mant;
 
 	add_f32 add_f32_1(.a(a), .b(b), .sum(sum)
-		, .a_greater(a_greater), .exp_diff(exp_diff), .sum_exp(rsltexp), .sum_mant({X,rsltmant})
-		, .mant_sum(prersltmant), .out1_mant(in1_mant), .out2_mant(in2_mant));
+		, .exp_diff(exp_diff), .sum_exp(rsltexp), .in_exp(prersltexp), .sum_mant({X,rsltmant})
+		, .mant_sum(prersltmant), .out1_mant(in1_mant), .out2_mant(in2_mant), .mant_sum_shift(mant_shift));
 
 	initial
-		$monitor("At time a(%h)(%d %d %h) + b(%h)(%d %d %h) = rslt(%h) a_great(%d) expdiff(%d) exp(%d) mant(%h)(%h)(%h)(%h)",
-				a, a_sign, a_exp, a_mant, b, b_sign, b_exp, b_mant, sum, a_greater, exp_diff, rsltexp, rsltmant, prersltmant, in1_mant, in2_mant);
+		$monitor("At time a(%h)(%d %d %h) + b(%h)(%d %d %h) = rslt(%h) expdiff(%d) exp(%d)(%d) mantshift(%d) mant(%h)(%h)(%h)(%h)",
+				a, a_sign, a_exp, a_mant, b, b_sign, b_exp, b_mant, sum, exp_diff, rsltexp, prersltexp, mant_shift, rsltmant, prersltmant, in1_mant, in2_mant);
 
 endmodule //test
