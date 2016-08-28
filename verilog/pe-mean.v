a63a30d4d86eb0d8f799e700e9e527bbf18ef1a1
adragon202 <ithinkwemet@gmail.com>
2016-08-28 18:43:04 -0400

    Initial work on Square Root module and Mean Module since completing division module
    Signed-off-by: adragon202 <ithinkwemet@gmail.com>

--- verilog/pe-mean.v
+++ verilog/pe-mean.v
@@ -1,10 +1,20 @@
-module mean( a, b, mn);
+module mean2_f32( clk, rst, a, b, rdy, mean);
+	localparam WIDTH = 32;
 	//input declaration
-	input a, b;
+	input clk, rst;
+	input [WIDTH - 1:0] a;
+	input [WIDTH - 1:0] b;
 	//output declaration
-	output mn;
+	output rdy;
+	output [WIDTH - 1:0] mean;
 	//port data types
-	wire a, b, mn;
+	wire rdy;
+	wire [WIDTH - 1:0] mean;
+	wire [WIDTH - 1:0] num_sum;
+	wire [WIDTH - 1:0] den_2 = 32'h40000000; //float value 2
 	//code starts here
 
-endmodule //addbit
+	add_f32 addf32_num(.a(a), .b(b), .cin(1'b0), .sum(num_sum));
+	divide_f32 dividef32_mean(.clk(clk), .rst(rst), .num(num_sum), .den(den_2), .rdy(rdy), .quo(mean));
+
+endmodule //mean2_f32
--- verilog/pe-sqrt.v
+++ verilog/pe-sqrt.v
@@ -1,3 +1,102 @@
+module squareroot_f32(clk, rst, a, rdy, sqrt);
+	localparam WIDTH = 32;
+	//input declaration
+	input clk, rst;
+	input [WIDTH - 1:0] a;
+	//output declaration
+	output rdy;
+	output [WIDTH - 1:0] sqrt;
+	//port data types
+	wire [WIDTH - 1:0] a;
+	reg [WIDTH - 1:0] den, result, mn, sqrt;
+	//code starts here
+
+endmodule //squareroot_f32
+
+/*
+short isqrt(short num) {
+    short res = 0;
+    short bit = 1 << 14; // The second-to-top bit is set: 1 << 30 for 32 bits
+ 
+    // "bit" starts at the highest power of four <= the argument.
+    while (bit > num)
+        bit >>= 2;
+        
+    while (bit != 0) {
+        if (num >= res + bit) {
+            num -= res + bit;
+            res = (res >> 1) + bit;
+        }
+        else
+            res >>= 1;
+        bit >>= 2;
+    }
+    return res;
+}
+INPUTS:
+	clk = clock input to drive operations
+	rst = reset for new input
+	a = Number to find Square Root for
+OUTPUTS:
+	rdy = flag output is valid
+	sqrt = Square Root of input
+*/
+module squareroot_f32_digit_by_digit(clk, rst, a, rdy, sqrt);
+	localparam WIDTH = 32;
+	localparam EXPONENTWIDTH = 8;
+	localparam MANTISSAWIDTH = 23;
+	//input declaration
+	input clk, rst;
+	input [WIDTH - 1:0] a;
+	//output declaration
+	output rdy;
+	output [WIDTH - 1:0] sqrt;
+	//port data types
+	wire clk, rst;
+	wire [WIDTH - 1:0] a;
+	wire a_sign = a[WIDTH - 1];
+	wire [EXPONENTWIDTH - 1:0] a_exp = a[WIDTH - 2:MANTISSAWIDTH];
+	wire [MANTISSAWIDTH:0] a_mant = {1'b1,a[MANTISSAWIDTH - 1:0]};
+	wire [WIDTH - 1:0] sqrt;
+	wire sqrt_sign;
+	wire [EXPONENTWIDTH - 1:0] sqrt_exp;
+	reg [MANTISSAWIDTH - 1:0] sqrt_mant;
+	//Internal data types
+	wire exp_denorm;
+	reg rdy;
+	reg [WIDTH - 1:0] guess;
+	wire [WIDTH - 1:0] bit_init;
+	reg [WIDTH - 1:0] bit;
+	//code starts here
+
+	//============Resolve sign=================
+	assign sqrt_sign = a_sign; //No need to acknowledge imaginary numbers in this module.
+
+	//============Resolve exponent=================
+	//((exponent - 127) >> 1) + 127
+	//if exponent < 127 then result[7] == 0
+	adder8 add8_exp1(.a(a_exp), .b(8'd 129), .sum(exp_denorm)); // a_exp - 127
+	adder8 add8_exp2(.a({1'b0,exp_denorm[EXPONENTWIDTH-1:1]}), .b(8'd 127), .sum(sqrt_exp)); //(exp_denorm >> 1) + 127
+
+	//============Resolve Mantissa=================
+	//Initial Bit Value
+	highestpower32(.a({()a_mant}), b, rem)
+
+	always @(posedge clk or posedge rst) begin
+		if (rst) begin
+			// reset
+			rdy <= 0;
+			sqrt_mant <= 0;
+		end
+		else if (!rst && clk) begin
+			
+		end
+	end
+
+	assign sqrt = {sqrt_sign,
+		{(a_exp < 127) ? 1'b0 : sqrt_exp[EXPONENTWIDTH - 1], sqrt_exp[EXPONENTWIDTH - 2:0]}, //exponent < 127 then null highest bit
+		sqrt_mant};
+endmodule //squareroot_f32_digit_by_digit
 
 /*
 Uses approximation method
@@ -8,50 +107,51 @@ Uses approximation method
 #5. set den = mn
 #6. Repeat from step 2
 INPUTS:
+	clk = clock input to drive operations
+	rst = reset for new input
 	a = Number to find Square Root for
 OUTPUTS:
+	rdy = flag output is valid
 	sqrt = Square Root of input
 */
-module squareroot(clk, a, sqrt);
+module squareroot_f32_approximation(clk, rst, a, rdy, sqrt);
+	localparam WIDTH = 32;
 	//input declaration
-	input [63:0] a;
+	input clk, rst;
+	input [WIDTH - 1:0] a;
 	//output declaration
-	output [63:0] sqrt;
+	output rdy;
+	output [WIDTH - 1:0] sqrt;
 	//port data types
-	wire [63:0] a, sqrt;
-	reg [63:0] den, result, mn;
+	reg divide_rst
+	wire divide_rdy;
+	reg mean_rst
+	wire mean_rdy;
+	wire [WIDTH - 1:0] a;
+	reg [WIDTH - 1:0] den, result, mn, sqrt;
 	//code starts here
-	divide D1(a,den,result);
-	mean M1(den,result,mn);
+
+	//Evaluate for solution
+	divide_f32 divide_f32_1(.clk(clk), .rst(divide_rst), .num(a), .den(den), .rdy(divide_rdy), .quo(divide_result));
+	mean2_f32 M1(.clk(clk), .rst(mean_rst), .a(den), .b(result), .rdy(mean_rdy), .mean(mn));
+	always @(posedge clk or posedge rst) begin
+		if (rst) begin
+			// reset
+			
+		end
+		else if (!rst && clk) begin
+			mn = 32'd 2;
+			den = 0;
+			result = 9999;
+			while (den != result) begin
+				den = mn;
+			end
+			sqrt <= result;
+		end
+	end
 	always @ (posedge clk)
 	begin
-		mn = 64'd 2;
-		den = 0;
-		result = 9999;
-		while (den != result) begin
-			den = mn;
-		end
-		sqrt <= result;
 	end
 
-endmodule //squareroot
+endmodule //squareroot_f32_approximation
 
-/*
-Performs operation dvd=a/b in floating point
-INPUTS:
-	a = numerator
-	b = denominator
-OUTPUTS:
-	dvd = dividend
-*/
-module divide(a, b, dvd);
-	//input declaration
-	input [63:0] a, b; 
-	//output declaration;
-	output [63:0] dvd;
-	//port data types
-	wire [63:0] a, b, dvd;
-	//code starts here
-	assign dvd = a / b;
-
-endmodule //divide
